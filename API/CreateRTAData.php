9e22b386b89e8916879a0330b15ceca3cc39a65b
Wilber1987 <wilberj1987@gmail.com>
2021-11-29 13:30:15 -0600

    WPC

--- API/CreateRTAData.php
+++ API/CreateRTAData.php
@@ -3,6 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Content-Type: application/json; charset=utf-8');
+header("Content-Type: text/html;charset=utf-8");
 echo json_encode("post");
 $JSONData = file_get_contents("php://input");
 $Data = json_decode($JSONData);
